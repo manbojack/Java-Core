d58779bb7e612b8ccd426954b0f0caf4e3815e02
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-06 23:21:44 +0300

    updated

---
+++ src/modules/module2/chapter1/task7/Main.java
@@ -0,0 +1,8 @@
+package modules.module2.chapter1.task7;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.priceCalculation(2.3, 5));
+    }
+}
---
+++ src/modules/module2/chapter1/task7/Solution.java
@@ -0,0 +1,7 @@
+package modules.module2.chapter1.task7;
+
+public class Solution {
+    public double priceCalculation(double price, int count) {
+        return price * count;
+    }
+}

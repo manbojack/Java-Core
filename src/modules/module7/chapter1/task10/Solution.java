e001cb44d8029fce1116bed95d6b7bd10576b128
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-13 13:39:19 +0300

    added task 7.1.10

---
+++ src/modules/module7/chapter1/task10/Solution.java
@@ -0,0 +1,24 @@
+package modules.module7.chapter1.task10;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Solution {
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return x -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
+    }
+
+}
+
+

cee8cd2f4296440d21cc73710bab1548a8805341
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-14 16:16:33 +0300

    refactoring task 7.2.12

--- src/modules/module7/chapter2/task12/Solution.java
+++ src/modules/module7/chapter2/task12/Solution.java
@@ -1,18 +1,31 @@
 package modules.module7.chapter2.task12;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
     public static void main(String[] args) {
-        java.util.HashMap<String, Long> result = (java.util.HashMap<String, Long>)
-                new java.io.BufferedReader(new java.io.InputStreamReader(System.in, java.nio.charset.StandardCharsets.UTF_8))
-                        .lines().flatMap(line -> java.util.stream.Stream.of(line.split("[\\p{Punct}\\s]+")))
-                        .map(String::toLowerCase)
-                        .collect(java.util.stream.Collectors.groupingBy(java.util.function.Function.identity(), java.util.stream.Collectors.counting()))
-                        .entrySet()
-                        .stream()
-                        .sorted(java.util.Map.Entry.comparingByKey())
-                        .sorted(java.util.Collections.reverseOrder(java.util.Map.Entry.comparingByValue()))
-                        .limit(10)
-                        .collect(java.util.stream.Collectors.toMap(java.util.Map.Entry::getKey, java.util.Map.Entry::getValue, (e1, e2) -> e2, java.util.LinkedHashMap::new));
+        HashMap<String, Long> result = new java.io.BufferedReader(
+                new java.io.InputStreamReader(
+                        System.in, java.nio.charset.StandardCharsets.UTF_8)
+        )
+                .lines().flatMap(line -> Stream.of(line.split("[\\p{Punct}\\s]+")))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(10)
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e2, LinkedHashMap::new
+                        )
+                );
 
         result.forEach((key, value) -> System.out.println(key));
     }
